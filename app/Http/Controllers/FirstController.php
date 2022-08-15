4b0e4b00166c7fb251479793215565c292fabf02
7OUDA <you@example.com>
2022-08-15 14:52:52 +0300

    m

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FirstController extends Controller
+{
+    public function index(){
+        return "hello";
+    }
+
+    public function create(){
+    }
+
+    public function store(){   
+    }
+
+    public function edit(){     
+    }
+
+    public function update(){     
+    }
+
+    public function show(){     
+    }
+
+    public function delete(){     
+    }
+}
